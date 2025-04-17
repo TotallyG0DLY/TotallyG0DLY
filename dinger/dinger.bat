90f026d498b8c97a5f58e56a48b4f1c898fa99da
TotallyG0DLY <kilian911.o@gmail.com>
2025-04-17 13:42:25 +0200

    Add files via upload

---
+++ dinger/dinger.bat
@@ -0,0 +1,10 @@
+@echo off
+echo dinger
+echo ---------
+set /p ans=watcha wanna do?
+if %ans% == go echo GO JOHNY, GO GO!
+if %ans% == trick echo wanna see a magic trick? 
+if %ans% == trick ipconfig
+if %ans% == trick echo screenshotted! JACKPOT!!!
+if %ans% == trick start "" "%USERPROFILE%\Downloads\dinger\dontopenyet.mp3"
+pause
